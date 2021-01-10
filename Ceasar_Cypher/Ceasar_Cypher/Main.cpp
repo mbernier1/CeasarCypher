0c13057c949187076cc061519130187489c1d858
Branden Alder <Trustyn@users.noreply.github.com>
2021-01-09 16:15:22 -0800

    1-9-21 HW1 Progress
    Left off needing to encrypt the perfect world text document. Not sure if doing it by character in loop or using ascii.

--- Ceasar_Cypher/Ceasar_Cypher/Main.cpp
+++ Ceasar_Cypher/Ceasar_Cypher/Main.cpp
@@ -1,17 +1,123 @@
 #include <iostream>
+#include <iomanip>
+#include <fstream>
 using std::cout;
 using std::endl;
 using std::cin;
 using std::string;
+using std::ifstream;
+using std::ofstream;
 
+void encryption();
+void decryption();
 
 
 int main()
 {
-    cout << "hello world" << endl;
+    int  menu = 2;
+    cout << "Caesar Cipher" << endl;
+    cout << "Would you like to encrypt or decrypt a file?" << endl;
+    cout << "Please enter 1 for encrypt.\nPlease enter 2 for decrypt." << endl;
+    cin >> menu;
 
+        switch (menu) {
+        case 1:
+            encryption();
+            break;
+
+        case 2:
+            decryption();
+            break;
+
+        default:
+            cout << "Invalid Choice" << endl;
+            
+    }
     
 
     return 0;
 
 }
+
+void encryption() {
+    int key = 7;
+    string filename = "default.txt";
+    char ch;
+    
+    cout << "What is the key you would like to use?" << endl;
+    //cout << "If key is not chosen, 7 will default." << endl;
+    cin >> key;
+    
+    cout << "Please enter what the name of your .TXT file you would like to encrypt." << endl;
+    //cout << "If file is not entered, default file will be used." << endl;
+    cin >> filename;
+
+    //Add line for opening file you want
+    ifstream fin;
+    fin.open(filename);
+    if (fin.is_open()) {
+        //ifstream fin("filename", ifstream::in);
+        while (fin >> ch) {
+            //cout << ch;
+            //convert to ascii
+            //cypher math (Plaintext + key) MOD 26
+            //convert ascii back to plaintext
+            //convert to UPPER
+            //new char value to save to new file default_encrypted.txt
+
+        }
+        fin.close();
+    }
+    else
+        cout << "Error file didn't open" << endl;
+    
+
+
+}
+
+void decryption() {
+    string key;
+
+}
+
+
+
+//for i = 0 to msg[i] != '\0'
+//ch = msg[i]
+//encrypt for lowercase letter
+//if(ch >= 'a' and ch <= 'z')
+//ch = ch + key
+//if (ch > 'z')
+//ch = ch - 'z' + 'a' - 1
+//done
+//msg[i] = ch
+//encrypt for uppercase letter
+//else if (ch >= 'a' and ch <= 'z')
+//ch = ch + key
+//if (ch > 'z')
+//ch = ch - 'z' + 'a' - 1
+//done
+//msg[i] = ch
+//done
+//done
+//print encrypted message
+
+//for i = 0 to msg[i] != '\0'
+//ch = msg[i]
+////decrypt for lowercase letter
+//if (ch >= 'a' and ch <= 'z')
+//ch = ch - key
+//if (ch < 'a')
+//    ch = ch + 'z' - 'a' + 1
+//    done
+//    msg[i] = ch
+//    //decrypt for uppercase letter
+//else if (ch >= 'a' and ch <= 'z')
+//ch = ch + key
+//if (ch < 'a')
+//    ch = ch + 'z' - 'a' + 1
+//    done
+//    msg[i] = ch
+//    done
+//    done
+//    print decrypted message
