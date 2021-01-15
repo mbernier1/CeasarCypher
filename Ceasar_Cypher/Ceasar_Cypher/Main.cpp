cdc6e60c8dc685f3b2cd51658576899957d9f7aa
Mike <38372845+mbernier1@users.noreply.github.com>
2021-01-14 21:48:51 -0800

    made a lot of changes
    added decrypt no key function

--- Ceasar_Cypher/Ceasar_Cypher/Main.cpp
+++ Ceasar_Cypher/Ceasar_Cypher/Main.cpp
@@ -16,14 +16,18 @@ using std::rename;
 
 void Encryption();
 void Decryption();
+void DecryptNoKey();
 
 
 int main()
 {
-    int  menu = 2;
+    int  menu = 3;
     cout << "Caesar Cipher" << endl;
     cout << "Would you like to encrypt or decrypt a file?" << endl;
-    cout << "Please enter 1 for encrypt. (This will CLEAR any previously encrypted text.)\nPlease enter 2 for decrypt." << endl;
+    cout << "Please enter 1 for encrypt. (This will CLEAR any previously encrypted text.)\n"
+		 <<	"Please enter 2 for decrypt. (This will CLEAR any previously decrypted text.)\n"
+		 << "Please enter 3 for decrypt without a key. (This will CLEAR any previously decrypted text.)" << endl;
+	
     cin >> menu;
 
     switch (menu)
@@ -35,46 +39,42 @@ int main()
         case 2:
             Decryption();
             break;
+    	
+        case 3:
+            DecryptNoKey();
+            break;
 
         default:
             cout << "Invalid Choice" << endl;
-            
     }
     
     return 0;
 }
-void encryption() {
-     
-
-}
-
-
 
 void Encryption()
 {
-    int key;
+    int key = 0;
     char ch;
     char filename[100] = "";
-    char newfilename[20] = "encrypted_text.txt";
+    char newFilename[20] = "encrypted_text.txt";
     char result;
 
     ofstream outfile;
-    outfile.open(newfilename, ofstream::out | ofstream::trunc);
+    outfile.open(newFilename, ofstream::out | ofstream::trunc);
     outfile.close();
 
     //1. Read in Key.
     cout << "Please enter the key you would like to use for encryption. (0-25)" << endl;
-    cin >> key;
 
 	// this if statement is in case the user decides to use a number bigger than 25
 	if (key >= 26)
 	{
-		// modus by 26 cause that the max letters in alphabet
-		// this will keep the key in the 0-25 range
-        key %= 26;
-
+        cout << "That was an incorrect value. Please try again.";
+        cout << "Please enter the key you would like to use for encryption. (0-25)" << endl;
 	}
-    
+	
+    cin >> key;
+
     //2. Read in "USERS" name for text file.                                      //take out this stuff in ( ) when strcat works
     cout << "Please enter the name of the text file you would like to be encrypted. (example: file.txt)" << endl;
     cin >> filename;
@@ -87,7 +87,7 @@ void Encryption()
     {
         ifstream fin(filename);
         ofstream outfile;
-        outfile.open(newfilename, std::ios_base::app);
+        outfile.open(newFilename, std::ios_base::app);
         while (fin >> ch) 
         { 
             if (ch >= 97 && ch <= 122) {
@@ -126,31 +126,29 @@ void Encryption()
     
 }
 
-
 void Decryption()
 {
-    int key;
+    int key = 0;
     char ch;
     char filename[100] = "";
-    char newfilename[20] = "decrypted_text.txt";
+    char newFilename[20] = "decrypted_text.txt";
     char result;
 
     ofstream outfile;
-    outfile.open(newfilename, ofstream::out | ofstream::trunc);
+    outfile.open(newFilename, ofstream::out | ofstream::trunc);
     outfile.close();
 
     //1. Read in Key.
     cout << "Please enter the key you would like to use for decryption. (0-25)" << endl;
-    cin >> key;
 
     // this if statement is in case the user decides to use a number bigger than 25
     if (key >= 26)
     {
-        // modus by 26 cause that the max letters in alphabet
-        // this will keep the key in the 0-25 range
-        key %= 26;
-
+        cout << "That was an incorrect value. Please try again.";
+        cout << "Please enter the key you would like to use for decryption. (0-25)" << endl;
     }
+	
+    cin >> key;
 
     //2. Read in "USERS" name for text file.                                      
     cout << "Please enter the name of the text file you would like to decrypt. (example: file.txt)" << endl;
@@ -163,7 +161,7 @@ void Decryption()
     {
         ifstream fin(filename);
         ofstream outfile;
-        outfile.open(newfilename, std::ios_base::app);
+        outfile.open(newFilename, std::ios_base::app);
         while (fin >> ch)
         {
             if (ch >= 65 && ch <= 90) {
@@ -190,3 +188,86 @@ void Decryption()
         cout << "Error file didn't open." << endl;
     }
 }
+
+void DecryptNoKey()
+{
+    int key, i = 0;
+    char ch;
+    char filename[100] = "";
+    char newFilename[50] = "decrypted_no_key_text.txt";
+    //char result;
+    int keys[26] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 };
+    //string encryptedText = "";
+    char encryptedText[] = "\0";
+    char * ch_ptr = nullptr;
+
+    ofstream outfile;
+    outfile.open(newFilename, ofstream::out | ofstream::trunc);
+    outfile.close();
+
+    //1. Read in "USERS" name for text file.                                      
+    cout << "Please enter the name of the text file you would like to decrypt. (example: file.txt)" << endl;
+    cin >> filename;
+
+    int j = 0;
+	
+    //2. Encrypt text from file.
+    while (i <= 25)
+    {
+        ifstream fin;
+        fin.open(filename);
+        if (fin.is_open())
+        {
+            ifstream fin(filename);
+            ofstream outfile;
+            outfile.open(newFilename, std::ios_base::app);
+            // 1) select a key
+            // 2) decrypt using chosen key
+            // 3) check decrypted test in string for words: the is it and of
+            // 4) if no words are found rinse and repeat
+                //  else append to file
+
+            while (fin >> ch)
+            {
+                encryptedText[j] += int((ch - keys[i] + 65) % 26 + 65);
+                //cout << encryptedText << '\n';
+                j++;
+            }
+
+            ch_ptr = strstr(encryptedText, ("the", "it", "is", "and", "of"));
+        	
+            if (ch_ptr != nullptr)
+            {
+                cout << "Those words are not in here." << endl;
+            }
+            else
+            {
+                cout << "Here are the words we found." << endl;
+                cout << encryptedText[j];
+            }
+        	
+            if (outfile.is_open())
+            {
+                //encryptedText = tolower(encryptedText);
+
+                outfile << encryptedText;
+            }
+            else
+            {
+                cout << "Output file Error." << endl;
+            }
+        	
+            outfile.close();
+            fin.close();
+        }
+        else
+        {
+            cout << "Error file didn't open." << endl;
+        }
+    	//for (int ii = 0; ii < )
+        //delete[] encryptedText;
+        //cryptedText[j] = '\0';
+        i++;
+    }
+}
+
